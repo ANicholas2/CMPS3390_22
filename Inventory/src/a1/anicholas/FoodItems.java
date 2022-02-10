a2ad1ae434f86ca53251ec4673fbdb6e264ad626
ANicholas2 <anicholas@csub.edu>
2022-02-10 00:10:29 -0800

    a1 done

---
+++ Inventory/src/a1/anicholas/FoodItems.java
@@ -0,0 +1,15 @@
+package a1.anicholas;
+
+public enum FoodItems {
+    Apple,
+    Banana,
+    Orange,
+    Burrito,
+    Hamburger,
+    Salad,
+    Taco,
+    Coke,
+    Pepsi,
+    DrPepper,
+    RootBeer
+}
---
+++ Inventory/src/a1/anicholas/ToolUses.java
@@ -0,0 +1,11 @@
+package a1.anicholas;
+
+public enum ToolUses {
+    HitThings,
+    ScrewThings,
+    WrenchThings,
+    CutThings,
+    ChopThings,
+    PickThings,
+    DigThings
+}
