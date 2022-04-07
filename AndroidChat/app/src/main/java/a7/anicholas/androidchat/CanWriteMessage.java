a06e24554258c92ceee5d89bbc559413ab057ecf
Adam Nicholas <anicholas@csub.edu>
2022-04-06 20:01:34 -0700

    a7 done

---
+++ AndroidChat/app/src/main/java/a7/anicholas/androidchat/CanWriteMessage.java
@@ -0,0 +1,5 @@
+package a7.anicholas.androidchat;
+
+public interface CanWriteMessage {
+    public void writeMessage(String message);
+}
---
+++ AndroidChat/app/src/main/java/a7/anicholas/androidchat/MainActivity.java
@@ -0,0 +1,44 @@
+package a7.anicholas.androidchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtUserName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtUserName = findViewById(R.id.txtName);
+    }
+
+    public void onLoginClicked(View v) {
+        String userName = txtUserName.getText().toString();
+            Log.i("LOGIN", txtUserName.getText().toString());
+            boolean nameIsValid = userName.matches("^\\w{2,9}[a-zA-Z0-9]$");
+
+            if(nameIsValid) {
+                // Move to the next activity and chat
+                Log.i("LOGIN", "Name is valid");
+                Intent intent = ChatActivity.createIntent(this, userName);
+                startActivity(intent);
+            } else {
+                Log.i("LOGIN", "Name was NOT valid!");
+                Snackbar snackbar = Snackbar.make(txtUserName,
+                        "Username must be 3-10 characters long and alphanumeric only!",
+                        Snackbar.LENGTH_LONG);
+                snackbar.setDuration(5000);
+                snackbar.setAnchorView(txtUserName);
+                snackbar.show();
+            }
+    }
+}
